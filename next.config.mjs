0738762cbfb5034b75454c375c93ac871cf3c770
Lyceum <168800@gmail.com>
2026-02-19 07:00:49 +0000

    fix: add serverActions allowedOrigins to resolve CSRF error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverActions: {
+    allowedOrigins: [
+      'localhost:3000',
+      '*.vercel.app',
+    ],
+  },
+};
 
 export default nextConfig;
